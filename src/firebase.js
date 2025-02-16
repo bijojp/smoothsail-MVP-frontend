cf9a3f2e55f225d255beb42cad91f289e8fe53f4
bijojp <bijojp.me@gmail.com>
2025-02-16 17:08:09 +0530

    config removed

--- src/firebase.js
+++ src/firebase.js
@@ -7,16 +7,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB38gKylZsqw6sCWJRcIqhyYm7dcLUIZJI",
-  authDomain: "smoothsail-mvp.firebaseapp.com",
-  projectId: "smoothsail-mvp",
-  storageBucket: "smoothsail-mvp.firebasestorage.app",
-  messagingSenderId: "909265753647",
-  appId: "1:909265753647:web:204d9ca67ca21ff03b18da",
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
